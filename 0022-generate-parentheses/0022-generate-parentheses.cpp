a122f77db40fe90b0992a03616fe0c6b21f62bb0
Abhicode-x <abhishekraj00078084@gmail.com>
2026-09-15 21:09:02 +0530

    Time: 4 ms (30.66%), Space: 13.9 MB (68.18%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+
+        function<void(string, int, int)> backtrack =
+            [&](string s, int open, int close) {
+
+            if (s.length() == 2 * n) {
+                ans.push_back(s);
+                return;
+            }
+
+            if (open < n) {
+                backtrack(s + "(", open + 1, close);
+            }
+
+            if (close < open) {
+                backtrack(s + ")", open, close + 1);
+            }
+        };
+
+        backtrack("", 0, 0);
+
+        return ans;
+    }
+};
